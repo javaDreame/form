b37dfb34a8c5a244b3be738b8683e068a4dc30bd
jack <jack@163.com>
2020-11-24 00:10:52 +0800

    test commit

--- src/jquery.form.js
+++ src/jquery.form.js
@@ -25,6 +25,7 @@
 (function (factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
+		//test commit
 		define(['jquery'], factory);
 	} else if (typeof module === 'object' && module.exports) {
 		// Node/CommonJS
